4a7f117bfece69de3d59620f68a18db7fe0839eb
michael7412541 <michael7412541@gmail.com>
2025-05-25 08:27:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (8.71%)

--- my-submissions/0001-two-sum/solution.c
+++ my-submissions/0001-two-sum/solution.c
@@ -1,25 +1,78 @@
-
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    *returnSize = 2;
-    int i, j, temp;
-    int *result = NULL;
-    //int *result = (int*)malloc(sizeof(int)*2);
-    //int *result=(int*)malloc(2*sizeof(int)),i,j,temp;
-    temp = target;
-    for(i = 0 ; i < numsSize ; i++){
-        temp = target - nums[i];
-        for(j = i + 1 ; j < numsSize ; j++){
-            if (temp - nums[j]== 0){
-                result = (int*)malloc(sizeof(int) * 2);
-                result[0] = i;
-                result[1] = j;
-                return result;
-            }
+#define BUCKETSIZE 1000
+
+typedef struct Node {
+    int val;
+    int index;
+    struct Node *next;
+} Node;
+
+int hash(int key) {
+    return abs(key) % BUCKETSIZE;
+}
+
+typedef struct hashmap{
+    struct Node *Bucket[BUCKETSIZE];
+} hashmap;
+
+void add_map(hashmap *map, int key, int index) {
+    int idx = hash(key);
+
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->index = index;
+    newNode->val = key;
+    newNode->next = map->Bucket[idx];
+    map->Bucket[idx] = newNode;
+}
+
+int check_map(hashmap *map, int key) {
+    int idx = hash(key);
+    Node *curr = map->Bucket[idx];
+    while(curr != NULL) {
+        if(curr->val == key) {
+            return curr->index;
         }
+        curr= curr->next;
     }
-    return 0;
+    return -1;
+}
+
+void freemap(hashmap *map) {
+    Node *curr, *prev = NULL;
+    int i = 0;
+    for(i = 0; i < BUCKETSIZE; i++) {
+        curr = map->Bucket[i];
+        while(curr != NULL) {
+            prev = curr;
+            curr = curr->next;
+            free(prev);
+        }
+        
+    }
+}
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    if(nums == NULL || numsSize == 0)
+        return NULL;
+    int i = 0, temp;
+    int* answer = (int*)malloc(sizeof(int)*2);
+    hashmap *map = (hashmap*)malloc(sizeof(hashmap));
+    for(i = 0; i < BUCKETSIZE; i++) {
+        map->Bucket[i] = NULL;
+    }
+    for(i = 0; i < numsSize; i++) {
+        temp = check_map(map, target - nums[i]); 
+        if(temp >= 0) {
+            freemap(map);
+            answer[0] = i;
+            answer[1] = temp;
+            *returnSize = 2;
+            return answer;
+        }
+        add_map(map, nums[i], i);
+        //printf("temp = %d, add nums[%d] = %d \n", temp, i, nums[i]);
+    }
+    freemap(map);
+    return NULL;
 }
