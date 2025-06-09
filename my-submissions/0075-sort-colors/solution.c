4f762247c83966cc5b0cd9251aeb5b86c7afec4a
michael7412541 <michael7412541@gmail.com>
2025-06-09 15:53:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (100.00%)

--- my-submissions/0075-sort-colors/solution.c
+++ my-submissions/0075-sort-colors/solution.c
@@ -1,7 +1,28 @@
-int compare_ints(const void *a, const void *b)
-{
-    return *(int*)a - *(int*)b;
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int findmin(int *nums, int start, int size) {
+    int i = 0, index = 0, min = 10;
+    for(i = start; i < size; i++)
+    {
+        if(nums[i] < min) {
+            min = nums[i];
+            index = i;
+        }
+            
+    }
+    return index;
 }
 void sortColors(int* nums, int numsSize) {
-    qsort(nums, numsSize, sizeof(int), compare_ints);
+    if(nums == NULL || numsSize == 0)
+        return;
+    int i = 0, minIndex = 0;
+    for(i = 0; i < numsSize; i++)
+    {
+        minIndex = findmin(nums, i, numsSize);
+        swap(&nums[i], &nums[minIndex]);
+    }
 }
