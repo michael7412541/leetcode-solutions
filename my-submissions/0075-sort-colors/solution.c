f60b93c5c030c4bd6d5d1428e14d8cd777328a18
michael7412541 <michael7412541@gmail.com>
2025-06-24 15:10:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (100.00%)

--- my-submissions/0075-sort-colors/solution.c
+++ my-submissions/0075-sort-colors/solution.c
@@ -1,15 +1,22 @@
 void sortColors(int* nums, int numsSize) {
     int i = 0, j = 0, count = 0, counting[3] = {0};
+    int* output = (int*)malloc(sizeof(int)*numsSize);
     for(i = 0; i < numsSize; i++)
     {
         counting[nums[i]]++;
     }
-    for(i = 0; i < 3; i++)
+    for(i = 1; i < 3; i++)
     {
-        for(j = 0; j < counting[i]; j++)
-        {
-            nums[count] = i;
-            count++;
-        }
+        counting[i] += counting[i-1];
     }
+    for(i = numsSize - 1; i >= 0; i--)
+    {
+        output[counting[nums[i]] - 1] = nums[i];
+        counting[nums[i]]--;
+    }
+    for(i = 0; i < numsSize; i++)
+    {
+        nums[i] = output[i];
+    }
+    free(output);
 }
