0549cb44ea4f1c2ba95f04f8f7a4c7245190aba8
michael7412541 <michael7412541@gmail.com>
2025-08-15 13:06:35 +0000

    Sync LeetCode submission Runtime - 22 ms (37.47%), Memory - 13.5 MB (5.11%)

--- my-submissions/0200-number-of-islands/solution.c
+++ my-submissions/0200-number-of-islands/solution.c
@@ -1,47 +1,48 @@
+typedef struct {
+    int x;
+    int y;
+} Point;
+
 int numIslands(char** grid, int gridSize, int* gridColSize) {
+    int **record = (int**)malloc(sizeof(int*)*gridSize);
+    int i = 0, j = 0, k, count = 0, head = 0, tail = 0;
+    Point *queue = (Point*)malloc(sizeof(Point)*gridSize*gridColSize[0]);
     
-    int i = 0, j = 0, k = 0, x, y, cx, cy, top = -1,count = 0;
-    int **stack = (int**)malloc(sizeof(int*)*gridSize* gridColSize[0]);
-    int dir[4][2] = {{1,0}, {0,1}, {-1,0}, {0, -1}};
-    for(i = 0; i < gridSize* gridColSize[0]; i++)
-            stack[i] = (int*)malloc(sizeof(int)*2);
+    for(i = 0; i < gridSize; i++) {
+        record[i] = (int*)calloc(gridColSize[0], sizeof(int));
+    }
+    int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
     
-    
-    for(i = 0; i < gridSize; i++){
-        for(j = 0; j < gridColSize[i]; j++){
-            if(grid[i][j] == '1'){
-                top++;
-                stack[top][0] = i;
-                stack[top][1] = j;
+    for(i = 0; i < gridSize; i++) {
+        for(j = 0; j < gridColSize[0]; j++) {
+            if(grid[i][j] == '1' && record[i][j] == 0) {
+                record[i][j] = 1;
+                queue[tail++] = (Point){i,j};
                 count++;
-                while(top >= 0){
-                    cx = stack[top][0];
-                    cy = stack[top][1];
-                    grid[cx][cy] = '0';
-                    top--;
+                while(head < tail) {
+                    Point p;
+                    p = queue[head++];
                     
-                    for(k = 0; k < 4; k++){
-                        x = cx + dir[k][0];
-                        y = cy + dir[k][1];
-                        if(x >= 0 && x < gridSize && y >= 0 && y < gridColSize[x] && grid[x][y] == '1'){
-                            top++;
-                            stack[top][0] = x;
-                            stack[top][1] = y;
+                    for(k = 0; k < 4; k++) {
+                        int r = p.x + dir[k][0];
+                        int c = p.y + dir[k][1];
+                        if(r >= 0 && r < gridSize && c >= 0 && c < gridColSize[0] && 
+                        grid[r][c] == '1' && record[r][c] == 0) {
+                            record[r][c] = 1;
+                            queue[tail++] = (Point){r,c};
                         }
+                        
                     }
                 }
-                    
                 
             }
         }
     }
+   for(i = 0; i < gridSize; i++) {
+        free(record[i]);
+    }
     
-    
-    for(i = 0; i < gridSize * gridColSize[0]; i++)
-    free(stack[i]);
-    free(stack);
-    
-    
+    free(record);
+    free(queue);
     return count;
-    
 }
