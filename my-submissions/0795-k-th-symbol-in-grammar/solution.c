56cdb3985554dba1ca874eea604bff5e03c535ac
michael7412541 <michael7412541@gmail.com>
2025-09-16 14:27:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (100.00%)

---
+++ my-submissions/0795-k-th-symbol-in-grammar/solution.c
@@ -0,0 +1,12 @@
+int kthGrammar(int n, int k) {
+    if(k == 1)
+        return 0;
+    if(n == 2 && k == 2)
+        return 1;
+    if(k % 2 == 0) {
+        return kthGrammar(n - 1, k/2)? 0 : 1;
+    }
+    else {
+        return kthGrammar(n - 1, (k+1)/2);
+    }
+}
