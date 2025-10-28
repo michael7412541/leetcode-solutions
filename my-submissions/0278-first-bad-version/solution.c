97791d641c873ef9b0c509e1ed0791e7893b097c
michael7412541 <michael7412541@gmail.com>
2025-10-28 16:32:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (100.00%)

---
+++ my-submissions/0278-first-bad-version/solution.c
@@ -0,0 +1,19 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+bool isBadVersion(int version);
+int firstBadVersion(int n) {
+    int left = 1, right = n, middle = 0;
+    while(left < right){
+        middle = left + (right - left)/2;
+        if(isBadVersion(middle) == 1){
+            right = middle;
+        }
+        else{
+            left = middle + 1;
+        }
+    }
+    if(left == right)
+        return left;
+    else
+        return 1;
+}
