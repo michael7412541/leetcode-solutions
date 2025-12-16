2c631cc036824a676deb839eb21cb55b17027ab9
michael7412541 <michael7412541@gmail.com>
2025-12-16 15:35:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.5 MB (100.00%)

---
+++ my-submissions/0019-remove-nth-node-from-end-of-list/solution.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    struct ListNode dummy;
+    dummy.next = head;
+    struct ListNode *slow = &dummy;
+    struct ListNode *fast = &dummy;
+    
+    for(int i = 0; i < n; i++){
+        fast = fast->next;
+    }
+    
+    while(fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next;
+    }
+    slow->next = slow->next->next;
+    return dummy.next;
+    
+}
