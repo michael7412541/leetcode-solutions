dbad48b83b9f5b5b9f14ac4367ed1fdfef4e4146
michael7412541 <michael7412541@gmail.com>
2026-03-01 14:55:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.5 MB (7.47%)

--- my-submissions/0144-binary-tree-preorder-traversal/solution.c
+++ my-submissions/0144-binary-tree-preorder-traversal/solution.c
@@ -9,7 +9,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-void preorder(struct TreeNode *node, int *arr, int *count) {
+/*void preorder(struct TreeNode *node, int *arr, int *count) {
     if(node == NULL)
         return;
     arr[(*count)] = node->val;
@@ -27,4 +27,57 @@ int* preorderTraversal(struct TreeNode* root, int* returnSize) {
     *returnSize = count;
     return arr;
         
+}*/
+
+/*int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+    if(root == NULL){
+        *returnSize = 0;
+        return NULL;
+    }
+    
+    struct TreeNode *stack[100];
+    struct TreeNode *cur = root;
+    int top = -1, count = 0;
+    int *answer = (int*)malloc(sizeof(int) * 100);
+    
+    stack[++top] = root;
+    
+    while(top >= 0){
+        cur = stack[top--];
+        answer[count++] = cur->val;
+        
+        if(cur->right)
+            stack[++top] = cur->right;
+        
+        if(cur->left)
+            stack[++top] = cur->left;
+        
+
+        
+        
+    }
+    *returnSize = count;
+    return answer;
+}*/
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+    struct TreeNode *cur = root;
+    struct TreeNode *stack[100];
+    int *answer = malloc(sizeof(int) * 100);
+    int top = -1, count = 0;
+    
+    while(cur || top != -1){
+        while(cur){
+            stack[++top] = cur;
+            answer[count++] = cur->val;
+            cur = cur->left;
+        }
+        
+        cur = stack[top--];
+        
+        cur = cur->right;
+    }
+    *returnSize = count;
+    return answer;
 }
+
