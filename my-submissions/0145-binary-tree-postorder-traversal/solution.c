2d6ab495abf069b32d07bb00c544d81e0ef60b6d
michael7412541 <michael7412541@gmail.com>
2026-03-02 14:31:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.5 MB (14.84%)

--- my-submissions/0145-binary-tree-postorder-traversal/solution.c
+++ my-submissions/0145-binary-tree-postorder-traversal/solution.c
@@ -10,7 +10,7 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
-void postorder(struct TreeNode *node, int *arr, int *count) {
+/*void postorder(struct TreeNode *node, int *arr, int *count) {
     if(node == NULL)
         return;
     
@@ -28,6 +28,33 @@ int* postorderTraversal(struct TreeNode* root, int* returnSize) {
     
     *returnSize = count;
     return arr;
+}*/
+
+int* postorderTraversal(struct TreeNode* root, int* returnSize){
+    struct TreeNode *stack[100];
+    struct TreeNode *cur = root, *prev = NULL;
+    int *answer = malloc(sizeof(int) * 100);
+    int top = -1, count = 0;
+    
+    while(cur != NULL || top != -1){
+        while(cur){
+            stack[++top] = cur;
+            cur = cur->left;
+        }
+        
+        struct TreeNode *node = stack[top];
+        if(node->right != NULL && prev != node->right){
+            cur = node->right;    
+        }
+        else{
+            prev = node;
+            answer[count++] = node->val;
+            top--;
+        }
+        
+    }
+    *returnSize = count;
+    return answer;
 }
 
 
