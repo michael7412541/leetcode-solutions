7a7a603b941c29256853555810bd20a76acab243
michael7412541 <michael7412541@gmail.com>
2026-03-07 08:50:48 +0000

    Sync LeetCode submission Runtime - 3 ms (97.56%), Memory - 11.1 MB (9.54%)

---
+++ my-submissions/0117-populating-next-right-pointers-in-each-node-ii/solution.c
@@ -0,0 +1,39 @@
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     struct Node *left;
+ *     struct Node *right;
+ *     struct Node *next;
+ * };
+ */
+
+struct Node* connect(struct Node* root) {
+	if(root == NULL)
+        return NULL;
+    
+    struct Node *queue[6000], *prev = NULL, *cur;
+    int level = 0, head = 0, tail = 0;
+    queue[tail++] = root;
+    while(head < tail){
+        int levelsize = tail - head;
+        //prev = NULL;
+        for(int i = 0; i < levelsize; i++){
+            cur = queue[head++];
+            if(i > 0)
+                prev->next = cur;
+         
+            if(cur->left)
+                queue[tail++] = cur->left;
+            if(cur->right)
+                queue[tail++] = cur->right;
+            
+            prev = cur;
+        }
+
+        cur->next = NULL;
+        
+    }
+    
+    return root;
+}
