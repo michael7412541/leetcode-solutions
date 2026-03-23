42c4ea66f70c17854fd4458d88282a377bcba6e9
michael7412541 <michael7412541@gmail.com>
2026-03-23 15:47:32 +0000

    Sync LeetCode submission Runtime - 1 ms (3.14%), Memory - 8.6 MB (98.65%)

--- my-submissions/0748-largest-number-at-least-twice-of-others/solution.c
+++ my-submissions/0748-largest-number-at-least-twice-of-others/solution.c
@@ -1,20 +1,18 @@
 int dominantIndex(int* nums, int numsSize) {
-    int max1 = 0, max2 = 0;
-    int i = 0,index = 0;
-    for(i = 0; i < numsSize; i++)
-    {
-        if(nums[i] > max1)
-        {
+    if(numsSize < 0)
+        return -1;
+    int max1 = -1, max2 = -1, index = 0;
+    for(int i = 0; i < numsSize; i++){
+        if(max1 < nums[i]){
             max2 = max1;
             max1 = nums[i];
             index = i;
         }
-        else if(nums[i] > max2)
-        {
+        else if(max2 < nums[i]){
             max2 = nums[i];
         }
     }
-    if(max1 >= 2*max2)
+    if(max1 >= max2 * 2)
         return index;
     else
         return -1;
