4408cdeba0d4021e8fab66447206eafa1a6734f5
michael7412541 <michael7412541@gmail.com>
2026-03-25 17:16:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.1 MB (32.03%)

--- my-submissions/0054-spiral-matrix/solution.c
+++ my-submissions/0054-spiral-matrix/solution.c
@@ -2,45 +2,30 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
-    int* answer = (int*)malloc(matrixSize * matrixColSize[0] * sizeof(int));
-    int top = 0, bottom = matrixSize - 1, left = 0, right = matrixColSize[0] - 1;
+    int left = 0, right = matrixColSize[0] - 1, up = 0, down = matrixSize - 1;
     int i = 0, j = 0, count = 0;
-    while(top <= bottom && left <= right)
-    {
-    if(top <= bottom && left <= right){
-        for(i = top, j = left; j <= right; j++)
-        {
-            answer[count] = matrix[i][j];
-            printf(" %d", answer[count]);
-            count++;
+    int *answer = malloc(sizeof(int) * matrixSize * matrixColSize[0]);
+    
+    while(left <= right && up <= down){
+        for(i = up, j = left; j <= right; j++)
+            answer[count++] = matrix[i][j];
+        
+        for(i = up + 1, j = right ; i <= down; i++)
+            answer[count++] = matrix[i][j];
+        
+        if (up < down) {
+        for(i = down, j = right - 1; j >= left; j-- )
+            answer[count++] = matrix[i][j];
         }
-        top++;}
-    if(top <= bottom && left <= right){
-        for(j = right, i = top; i <= bottom; i++)
-        {
-            answer[count] = matrix[i][j];
-            printf(" %d", answer[count]);
-            count++;
+        if (left < right){
+        for(i = down - 1, j = left; i > up; i--)
+            answer[count++] = matrix[i][j];
         }
-        right--;}
-    if(top <= bottom && left <= right){
-        for(j = right, i = bottom; j >= left; j--)
-        {
-            answer[count] = matrix[i][j];
-            printf(" %d", answer[count]);
-            count++;
-        }
-        bottom--;}
-    if(top <= bottom && left <= right){
-        for(j = left, i = bottom; i >= top; i--)
-        {
-            answer[count] = matrix[i][j];
-            printf(" %d", answer[count]);
-            count++;
-        }
-        left++;}
-       
+        up++;
+        down--;
+        left++;
+        right--;
     }
-    *returnSize = count;
+    *returnSize = matrixSize * matrixColSize[0];
     return answer;
 }
