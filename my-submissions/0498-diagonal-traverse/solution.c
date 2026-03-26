2502a5cc950d97d177007cb8d92ed77463136745
michael7412541 <michael7412541@gmail.com>
2026-03-26 14:50:57 +0000

    Sync LeetCode submission Runtime - 100 ms (2.17%), Memory - 20.9 MB (46.74%)

--- my-submissions/0498-diagonal-traverse/solution.c
+++ my-submissions/0498-diagonal-traverse/solution.c
@@ -2,40 +2,30 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findDiagonalOrder(int** mat, int matSize, int* matColSize, int* returnSize) {
-    int* answer = (int*)malloc(matSize*matColSize[0]*sizeof(int));
-    int i = 0, j = 0, count = 0;
-    int change = 0;
-    for(i = 0; i <= matSize + matColSize[0] - 2; i++)
-    {
-        if(change == 0)
-        {
-            for(j = 0; j <= i ; j++)
-            {
-                if(i-j < matSize && j < matColSize[0])
-                {
-                    answer[count] = mat[i-j][j];
-                    printf("mat = %d",  mat[i-j][j]);
-                    printf("answer = %d\n",  answer[count]);
-                    count++;
-                }
-            }
-            change = 1;
+    int dir = 1, count = 0, j = 0;
+    int *answer = malloc(sizeof(int) * matColSize[0] * matSize);
+    for(int k = 0; k < matSize + matColSize[0] - 1; k++){
+        if(dir==0){
+        for(int i = 0; i <= k; i++){
+            j = k - i;
+            if(i < matSize && j < matColSize[0])
+                answer[count++] = mat[i][j];
         }
-        else
-        {
-            for(j = i; j >= 0 ;j--)
-            {
-                if(i-j < matSize && j < matColSize[0])
-                {
-                    answer[count] = mat[i-j][j];
-                    printf("mat = %d",  mat[i-j][j]);
-                    printf("answer = %d\n",  answer[count]);
-                    count++;
-                }
-            }
-            change = 0;
+            dir = 1;
         }
+        else{
+            for(int i = k; i >= 0; i--){
+            j = k - i;
+            if(i < matSize && j < matColSize[0])
+                answer[count++] = mat[i][j];
+        }
+            dir = 0;
+        }
+            
+        
+        
     }
-    *returnSize = matSize*matColSize[0];
+    *returnSize = matSize * matColSize[0];
+    
     return answer;
 }
