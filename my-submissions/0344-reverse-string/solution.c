deb5c5330f9b7bf2f0c9949e4cce11dcff9132e2
michael7412541 <michael7412541@gmail.com>
2026-03-26 16:47:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (75.41%)

--- my-submissions/0344-reverse-string/solution.c
+++ my-submissions/0344-reverse-string/solution.c
@@ -1,20 +1,12 @@
-void swap(char *a, char *b) {
-    char temp;
-    temp = *a;
+void swap(char *a, char *b){
+    char temp = *a;
     *a = *b;
     *b = temp;
 }
-void reverse(char *s, int size, int p) {
-    if(p >= size/2)
-        return;
-    else {
-        swap(&s[p], &s[size - p - 1]);
-        reverse(s, size, p + 1);
-    }
-}
 void reverseString(char* s, int sSize) {
-    
-    reverse(s, sSize, 0);
+    for(int i = 0 ; i < sSize/2 ; i++){
+        swap(&s[i], &s[sSize-i-1]);
+    }
 }
 
 
