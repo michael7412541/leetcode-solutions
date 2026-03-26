9c0c6053c025329f648924c46ae5621c198fb6bc
michael7412541 <michael7412541@gmail.com>
2026-03-26 15:31:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (90.12%)

--- my-submissions/0067-add-binary/solution.c
+++ my-submissions/0067-add-binary/solution.c
@@ -1,58 +1,34 @@
 char* addBinary(char* a, char* b) {
-    if(a == NULL || b == NULL)
-        return NULL;
-    
-    int len_a = strlen(a);
-    int len_b = strlen(b);
-    int i = 0, j = 0, k = 0, carry = 0;
-    int c1, c2, c3, temp = 0;
-    char* answer;
-    int max_len = (len_a > len_b ? len_a : len_b);
-    answer = (char*)malloc((max_len + 2) * sizeof(char)); // +1 for carry, +1 for '\0'
-    answer[max_len + 1] = '\0'; // Null terminator
-    k = max_len;
-    i = len_a - 1;
-    j = len_b - 1;
-
-    while(j >= 0 || i >= 0)
-    {
-        c1 = (i >= 0 && a[i] == '1')? 1 : 0;
-        c2 = (j >= 0 && b[j] == '1')? 1 : 0;
-        c3 = (carry == 1)? 1 : 0;
-        temp = c1 + c2 + c3;
-        if(temp == 1)
-        {
-            carry = 0;
-            answer[k] = '1';
-        }
-        else if(temp == 2)
-        {
-            carry = 1;
-            answer[k] = '0';
-        }
-        else if(temp == 3)
-        {
-            carry = 1;
-            answer[k] = '1';
-        }
-        else//temp ==0
-        {
-            carry = 0;
-            answer[k] = '0';
-        }
+    int len_a = strlen(a), len_b = strlen(b);
+    int len = (len_a > len_b) ? len_a + 1 : len_b + 1;
+    int i = len_a - 1, j = len_b - 1, carry = 0;
+    char *answer = malloc(sizeof(char) * (len + 1));
+    answer[len] = '\0';
+    int k = len - 1;
+    while(i >= 0 || j >= 0 || carry > 0){
         
-        k--;
-        j--;
+        int temp = carry + (i >= 0 ? a[i] - '0' : 0) + (j >= 0 ? b[j] - '0' : 0);
+        if(temp == 3){
+            carry = 1;
+            answer[k--] = '1';
+        }
+        else if(temp == 2){
+            carry = 1;
+            answer[k--] = '0';
+        }
+        else{
+            carry = 0;
+            answer[k--] = temp + '0';
+        }
         i--;
+        j--;
     }
     
-    
-    if(carry == 1)
-    {
-        answer[k] = '1';
-        return &answer[k];
+   if (k >= 0) {
+        int offset = k + 1; // 這是真正字串開始的位置
+        for (int m = 0; m <= (len - offset); m++) {
+            answer[m] = answer[m + offset];
+        }
     }
-    else
-        return &answer[k+1];
-    
+    return answer;
 }
