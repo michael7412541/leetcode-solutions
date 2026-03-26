d281cb461ff7ebe493ad4550c657dfd73ea2c34d
michael7412541 <michael7412541@gmail.com>
2026-03-26 14:20:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.7 MB (35.75%)

--- my-submissions/0118-pascals-triangle/solution.c
+++ my-submissions/0118-pascals-triangle/solution.c
@@ -4,31 +4,26 @@
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
-    int** answer = (int**)malloc(numRows * sizeof(int*));
-    *returnColumnSizes = (int*)malloc(numRows * sizeof(int)); 
-    int i = 0, j = 0;
+    *returnSize = numRows;
+    int **answer = malloc(sizeof(int*) * (*returnSize));
     
-    for(i = 1; i <= numRows; i++)
-    {
-        answer[i-1] = (int*)malloc(i * sizeof(int));
-        (*returnColumnSizes)[i-1] = i;
-        for(j = 0; j < i; j++)
-        {
-            if(i == 1)
-                answer[i-1][j] = 1;
-            else if(i == 2)
-                answer[i-1][j] = 1;
-            else if(j == 0 || j == i - 1)
-                answer[i-1][j] = 1;
+    // ⭐ 補這一行
+    *returnColumnSizes = malloc(sizeof(int) * numRows);
+    for(int i = 0; i < numRows; i++){
+        (*returnColumnSizes)[i] = i + 1;
+        answer[i] = malloc(sizeof(int) * (i + 1));
+    }
+    for(int i = 0; i < numRows; i++){
+        for(int j = 0; j < i + 1; j++){
+            if(i == 0 || j == 0 || j == i)
+                answer[i][j] = 1;
             else
-                answer[i-1][j] = answer[i-2][j] + answer[i-2][j-1];
-            printf(" %d", answer[i-1][j]);
+                answer[i][j] = answer[i-1][j] + answer[i-1][j-1];
         }
-        printf(" \n");
     }
     
-    
-    *returnSize = numRows;
-    
     return answer;
+    
 }
+
+
