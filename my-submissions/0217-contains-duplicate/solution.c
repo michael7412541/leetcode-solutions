8fcba57d3dfb183561378ce031be9afebbe732dd
michael7412541 <michael7412541@gmail.com>
2026-05-05 16:00:20 +0000

    Sync LeetCode submission Runtime - 65 ms (13.75%), Memory - 47.5 MB (5.05%)

--- my-submissions/0217-contains-duplicate/solution.c
+++ my-submissions/0217-contains-duplicate/solution.c
@@ -1,49 +1,25 @@
-#define BUCKETSIZE 1000
-typedef struct Node {
-    int val;
-    struct Node* next;
-} Node;
+//#include "uthash.h"
 
-typedef struct hashset {
-    struct Node* bucket[BUCKETSIZE];
-} hashset;
-
-int hash(int key) {
-    return abs(key) % BUCKETSIZE;
-}
-
-bool hashsetadd(hashset *obj, int key) {
-    Node *curr = obj->bucket[hash(key)];
-    while(curr != NULL) {
-        if(curr->val == key)
-            return true;
-        curr = curr->next;
-    }
-    Node *newNode = (Node*)malloc(sizeof(Node));
-    newNode->val = key;
-    newNode->next = obj->bucket[hash(key)];
-    obj->bucket[hash(key)] = newNode;
-    return false;
-}
-
-void hashsetinit(hashset *obj) {
-    int i = 0;
-    for(i = 0; i < BUCKETSIZE; i++) {
-        obj->bucket[i] = NULL;
-    }
-}
-
+typedef struct {
+    int key;
+    int count;
+    UT_hash_handle hh;
+} HashNode;
 bool containsDuplicate(int* nums, int numsSize) {
-    if(nums == NULL || numsSize == 0)
-        return false;
-    hashset* set = (hashset*)malloc(sizeof(hashset));
-    hashsetinit(set);
-    int i = 0;
-    bool temp = false;
-    for(i = 0; i < numsSize; i++) {
-        temp = hashsetadd(set, nums[i]);
-        if(temp == true)
+    HashNode *map = NULL;
+    for(int i = 0; i < numsSize; i++){
+        HashNode *node;
+
+        HASH_FIND_INT(map, &nums[i], node);
+        if(node != NULL){
             return true;
+        }
+        else{
+            node = malloc(sizeof(HashNode));
+            node->key = nums[i];
+            node->count = 1;
+            HASH_ADD_INT(map, key, node);
+        }
     }
     return false;
 }
