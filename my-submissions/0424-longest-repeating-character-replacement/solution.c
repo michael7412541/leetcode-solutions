75011ad19e591ca1593373ee17d187ed8dc4f28a
michael7412541 <michael7412541@gmail.com>
2026-05-12 16:14:52 +0000

    Sync LeetCode submission Runtime - 19 ms (16.58%), Memory - 9.5 MB (32.61%)

--- my-submissions/0424-longest-repeating-character-replacement/solution.c
+++ my-submissions/0424-longest-repeating-character-replacement/solution.c
@@ -1,5 +1,5 @@
-int maxhash(int * hash){
-    int max = -1;
+int max_hash(int *hash){
+    int max = INT_MIN;
     for(int i = 0; i < 26; i++){
         if(hash[i] > max)
             max = hash[i];
@@ -7,19 +7,17 @@ int maxhash(int * hash){
     return max;
 }
 int characterReplacement(char* s, int k) {
-    int right = 0, left = 0, size = strlen(s), hash[26] = {0}, max_length = INT_MIN;
+    int hash[26] = {0}, right = 0, left = 0, answer = 0;
+    int size = strlen(s);
     for(right = 0; right < size; right++){
         hash[s[right] - 'A']++;
-        if(right - left + 1 - maxhash(hash) <= k){
-            if(right - left + 1 > max_length)
-                max_length = right - left + 1;
-        }
-        while(right - left + 1 - maxhash(hash) > k && left <= right){
+        while(max_hash(hash) + k < (right - left + 1) && left <= right){
             hash[s[left] - 'A']--;
             left++;
+           
         }
-
-
+        if(right - left + 1 > answer)
+                answer = right - left + 1;
     }
-    return max_length;
+    return answer;
 }
