9401c90e03aa5d5175014e1c1b5b879bba5a349d
michael7412541 <michael7412541@gmail.com>
2026-05-19 14:45:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (68.43%)

--- my-submissions/0033-search-in-rotated-sorted-array/solution.c
+++ my-submissions/0033-search-in-rotated-sorted-array/solution.c
@@ -3,27 +3,32 @@ int search(int* nums, int numsSize, int target) {
     
     while(left <= right){
          mid  = left + (right - left)/2;
-        //printf("L:num[%d] = %d, R:num[%d] = %d, M:num[%d] = %d\n", left,nums[left],right,nums[right],mid,nums[mid]);
-       
+        
         if(target == nums[mid])
             return mid;
         
         if(nums[left] <= nums[mid]){
-            if(nums[left] <= target && target < nums[mid]){
+            if(target < nums[mid] && target > nums[left]){
                 right = mid - 1;
+            }
+            else if(target == nums[left]){
+                return left;
             }
             else{
                 left = mid + 1;
             }
         }
         else{
-            if(nums[mid] < target && target <= nums[right]){
+            if(target > nums[mid] && target < nums[right]){
                 left = mid + 1;
             }
+            else if(target == nums[right])
+                return right;
             else{
-               right = mid - 1; 
+                right = mid - 1;
             }
         }
     }
+
     return -1;
 }
