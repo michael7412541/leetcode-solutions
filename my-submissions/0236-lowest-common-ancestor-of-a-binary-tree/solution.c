cfc8be2bdcf472c13cc34d030a282fb4710961e2
michael7412541 <michael7412541@gmail.com>
2026-05-26 16:38:35 +0000

    Sync LeetCode submission Runtime - 26 ms (100.00%), Memory - 21.9 MB (100.00%)

--- my-submissions/0236-lowest-common-ancestor-of-a-binary-tree/solution.c
+++ my-submissions/0236-lowest-common-ancestor-of-a-binary-tree/solution.c
@@ -6,41 +6,18 @@
  *     struct TreeNode *right;
  * };
  */
-
-
-
 struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
-   /*(root == NULL)
-        return NULL;
-    else if(root == p)
-        return p;
-    else if(root == q)
-        return q;
-    else if(lowestCommonAncestor(root->left, p, q) != NULL && lowestCommonAncestor(root->right, p, q) != NULL){
-        return root;
-    }
-    else
-        return root;
-    */
     if(root == NULL)
         return NULL;
     
-    if(root == p || root == q)
+    if(root->val == p->val || root->val == q->val)
         return root;
-    
+
     struct TreeNode *left = lowestCommonAncestor(root->left, p, q);
     struct TreeNode *right = lowestCommonAncestor(root->right, p, q);
     
-    if(left == NULL && right == NULL)
-        return NULL;
-    else if(left == NULL && right != NULL){
-        return right;
-    }
-    else if(left != NULL && right == NULL){
-        return left;
-    }
-    else{
+    if(left != NULL && right != NULL)
         return root;
-    }
-    
+    else
+        return left == NULL ? right : left;
 }
