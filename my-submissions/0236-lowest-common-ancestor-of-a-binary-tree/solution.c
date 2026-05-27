736f55297dbd55645d47125a80700da6da795358
michael7412541 <michael7412541@gmail.com>
2026-05-27 16:46:55 +0000

    Sync LeetCode submission Runtime - 23 ms (100.00%), Memory - 22.1 MB (100.00%)

--- my-submissions/0236-lowest-common-ancestor-of-a-binary-tree/solution.c
+++ my-submissions/0236-lowest-common-ancestor-of-a-binary-tree/solution.c
@@ -7,17 +7,15 @@
  * };
  */
 struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
-    if(root == NULL)
-        return NULL;
-    
-    if(root->val == p->val || root->val == q->val)
+    if(root == NULL || root->val == p->val || root->val == q->val)
         return root;
-
+    
     struct TreeNode *left = lowestCommonAncestor(root->left, p, q);
     struct TreeNode *right = lowestCommonAncestor(root->right, p, q);
-    
+
     if(left != NULL && right != NULL)
         return root;
     else
         return left == NULL ? right : left;
+
 }
