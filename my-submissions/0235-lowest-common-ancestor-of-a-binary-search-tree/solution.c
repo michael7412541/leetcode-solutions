95cadbfd1ef8e1fec98740690c5bc63333de9f08
michael7412541 <michael7412541@gmail.com>
2026-05-28 16:55:50 +0000

    Sync LeetCode submission Runtime - 36 ms (34.84%), Memory - 29.2 MB (11.45%)

--- my-submissions/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.c
+++ my-submissions/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.c
@@ -8,28 +8,17 @@
  */
 
 struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
-    /*if(root == p || root == q || root == NULL)
+    if(root == NULL || root->val == p->val || root->val == q->val)
         return root;
-    
-    struct TreeNode *L = lowestCommonAncestor(root->left, p, q);
-    struct TreeNode *R = lowestCommonAncestor(root->right, p, q);
-    
-    if(L != NULL && R != NULL)
-        return root;
-    else
-        return L == NULL ? R : L;*/
-    if(root == NULL)
-        return NULL;
-    
-    int small = 0,large = 0;
-    small = p->val > q->val ? q->val : p->val;
-    large = p->val > q->val ? p->val : q->val;
-    if(root->val >= small && root->val <= large)
-        return root;
-    else if(root->val > small && root->val > large){
-        return lowestCommonAncestor(root->left, p, q); 
+
+
+    if(p->val < root->val && q->val < root->val){
+        return lowestCommonAncestor(root->left, p, q);
     }
-    else{
+    else if(p->val > root->val && q->val > root->val){
         return lowestCommonAncestor(root->right, p, q);
     }
-}                                                                                                                                                                                                                                                                                                      
+    else{
+        return root;
+    }
+}
