382f68329c8d7314770a1c56c051b48adde23594
michael7412541 <michael7412541@gmail.com>
2026-06-07 14:41:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.4 MB (31.20%)

--- my-submissions/0226-invert-binary-tree/solution.c
+++ my-submissions/0226-invert-binary-tree/solution.c
@@ -6,30 +6,17 @@
  *     struct TreeNode *right;
  * };
  */
-
-
-struct TreeNode* invertTree(struct TreeNode* root){
+struct TreeNode* invertTree(struct TreeNode* root) {
     if(root == NULL)
-        return root;
-    preorder_traversal(root);
+        return NULL;
+    
+    invertTree(root->left);
+    invertTree(root->right);
+
+    struct TreeNode *temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+
     return root;
 }
-
-void preorder_traversal(struct TreeNode* node){
-    if(node == NULL)
-        return ;
-    struct TreeNode *temp;
-    printf("%d ", node->val);
-    preorder_traversal(node->left);
-    preorder_traversal(node->right);
-    if(node->left != NULL || node->right != NULL){
-        temp = node->left;
-        node->left = node->right;
-        node->right = temp;
-    }
-    
-    
-    return ;
-}
-
-
