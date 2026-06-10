99f89090600d19aedd01ab5ac1936fc06e9b94f8
michael7412541 <michael7412541@gmail.com>
2026-06-10 15:26:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.8 MB (99.62%)

--- my-submissions/0035-search-insert-position/solution.c
+++ my-submissions/0035-search-insert-position/solution.c
@@ -1,19 +1,15 @@
-
-int binary_search(int *nums, int left, int right, int val){
-    int mid = left + (right - left)/2;
-    if(val == nums[mid] || left == right)
-        return mid;
-    else if(val > nums[mid])
-        return binary_search(nums, mid + 1, right, val);
-    else
-        return binary_search(nums, left, mid, val);
-}
 int searchInsert(int* nums, int numsSize, int target) {
-    if(target > nums[numsSize - 1])
-        return numsSize;
-    else if(target < nums[0])
-        return 0;
-    else
-        return binary_search(nums, 0, numsSize - 1, target);
+    if(nums == NULL || numsSize == 0)
+        return -1;
+    int left = 0, right = numsSize, mid = 0;
+    while(left < right){
+        mid = left + (right - left)/2;
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] > target)
+            right = mid;
+        else
+            left = mid + 1;
+    }
+    return left;
 }
-
