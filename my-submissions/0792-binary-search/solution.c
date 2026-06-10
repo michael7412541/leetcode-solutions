f7c59d63dd9978211bff70d210f39122c36616fd
michael7412541 <michael7412541@gmail.com>
2026-06-10 15:23:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (81.40%)

--- my-submissions/0792-binary-search/solution.c
+++ my-submissions/0792-binary-search/solution.c
@@ -1,17 +1,15 @@
 int search(int* nums, int numsSize, int target) {
-    int middle = 0, left = 0, right = numsSize - 1;
-    //if(numsSize == 1)
-      //  return target == nums[0] ? 0 : -1;
-    while(left <= right) {
-        middle = (right + left)/2;
-        if(target == nums[middle])
-            return middle;
-        else if(target > nums[middle]) {
-            left = middle + 1;
-        }
-        else {
-            right = middle - 1;
-        }
+    if(nums == NULL || numsSize == 0)
+        return -1;
+    int left = 0, right = numsSize - 1, mid = 0;
+    while(left <= right){
+        mid = left + (right - left)/2;
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] > target)
+            right = mid - 1;
+        else
+            left = mid + 1;
     }
     return -1;
 }
