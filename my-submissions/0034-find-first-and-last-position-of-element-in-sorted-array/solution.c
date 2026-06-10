2f81c20eef0dc0cc6b65db8564e9dd74c6b50df5
michael7412541 <michael7412541@gmail.com>
2026-06-10 15:46:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.6 MB (36.94%)

--- my-submissions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.c
+++ my-submissions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.c
@@ -1,54 +1,39 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
+int binary_search(int *nums, int numsSize, int target){
+    int left = 0, right = numsSize, mid = 0;
+    while(left < right){
+        mid = left + (right - left)/2;
+        if(nums[mid] >= target)
+            right = mid;
+        else
+            left = mid + 1;
+    }
+    return left;
+}
 int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
-    int *answer = (int*)malloc(sizeof(int)*2);
-    if(numsSize < 1){
-        *returnSize = 2;
+    
+    *returnSize = 2;
+    int *answer = malloc(sizeof(int) * 2);
+    if(nums == NULL || numsSize == 0){
         answer[0] = -1;
         answer[1] = -1;
         return answer;
     }
-    int L = 0, R = numsSize - 1, mid = 0;
-    int start = 0, end = 0;
-    while(L < R){
-        mid = L + (R - L)/2;
-        if(nums[mid] < target)
-            L = mid + 1;
-        else
-            R = mid;
-    }
-    if(nums[L] != target){
-        start = -1;
+
+    int position = binary_search(nums, numsSize, target);
+    printf("%d", position);
+    if( position == numsSize || nums[position] != target ){
+        answer[0] = -1;
+        answer[1] = -1;
     }
     else{
-        start = L;
+        answer[0] = position;
+        answer[1] = binary_search(nums, numsSize, target + 1) - 1;
+
     }
-    L = 0;
-    R = numsSize - 1;
-    while(L < R){
-        mid = L + (R - L)/2;
-        if(nums[mid] <= target)
-            L = mid + 1;
-        else
-            R = mid;
-    }
-    
-    if (L < numsSize && nums[L] <= target)
-        L++;
-    end = L - 1;
-    if(end < start) end = start;
-    if(end >= numsSize) end = numsSize - 1;
-    
-    
-    answer[0] = start;
-    if(start == -1)
-        answer[1] = -1;
-    else
-        answer[1] = end;
-    
-    *returnSize = 2;
+
     return answer;
-    
-        
 }
