0914c26966bbbbcb28f6771e472bd9214ac590ce
michael7412541 <michael7412541@gmail.com>
2026-06-15 14:09:55 +0000

    Sync LeetCode submission Runtime - 15 ms (94.24%), Memory - 13.2 MB (5.11%)

--- my-submissions/0200-number-of-islands/solution.c
+++ my-submissions/0200-number-of-islands/solution.c
@@ -1,48 +1,45 @@
 typedef struct {
     int x;
     int y;
-} Point;
-
+} Point ;
 int numIslands(char** grid, int gridSize, int* gridColSize) {
-    int **record = (int**)malloc(sizeof(int*)*gridSize);
-    int i = 0, j = 0, k, count = 0, head = 0, tail = 0;
-    Point *queue = (Point*)malloc(sizeof(Point)*gridSize*gridColSize[0]);
-    
-    for(i = 0; i < gridSize; i++) {
-        record[i] = (int*)calloc(gridColSize[0], sizeof(int));
-    }
-    int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
-    
-    for(i = 0; i < gridSize; i++) {
-        for(j = 0; j < gridColSize[0]; j++) {
-            if(grid[i][j] == '1' && record[i][j] == 0) {
-                record[i][j] = 1;
-                queue[tail++] = (Point){i,j};
-                count++;
-                while(head < tail) {
-                    Point p;
-                    p = queue[head++];
-                    
-                    for(k = 0; k < 4; k++) {
-                        int r = p.x + dir[k][0];
-                        int c = p.y + dir[k][1];
-                        if(r >= 0 && r < gridSize && c >= 0 && c < gridColSize[0] && 
-                        grid[r][c] == '1' && record[r][c] == 0) {
-                            record[r][c] = 1;
-                            queue[tail++] = (Point){r,c};
-                        }
-                        
-                    }
-                }
-                
-            }
+    int **record = malloc(sizeof(int*) * gridSize);
+    for(int i = 0; i < gridSize; i++){
+        record[i] = malloc(sizeof(int) * gridColSize[i]);
+        for(int j = 0; j < gridColSize[i]; j++){
+            record[i][j] = 0;
         }
     }
-   for(i = 0; i < gridSize; i++) {
-        free(record[i]);
+    int dir[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}}, answer = 0, head = -1, tail = -1, x1, y1;
+    Point queue[10000], cur;
+
+    for(int i = 0; i < gridSize; i++){
+        for(int j = 0; j < gridColSize[i]; j++){
+            
+            if(record[i][j] == 0 && grid[i][j] == '1'){
+                //printf("find\n");
+                head = -1;
+                tail = -1;
+                record[i][j] = 1;
+                queue[++tail] = (Point){i, j};
+
+                while(head < tail){
+                    cur = queue[++head];
+                    for(int k = 0; k < 4; k++){
+                        x1 = cur.x + dir[k][0];
+                        y1 = cur.y + dir[k][1];
+                        if(x1 >= 0 && x1 < gridSize && y1 >= 0 && y1 < gridColSize[0] && grid[x1][y1] == '1' && record[x1][y1] == 0){
+                            record[x1][y1] = 1;
+                            queue[++tail] = (Point){x1, y1};
+                        }
+                    }
+                }
+                answer++;
+                
+            }
+            
+        }
     }
-    
-    free(record);
-    free(queue);
-    return count;
+
+    return answer;
 }
