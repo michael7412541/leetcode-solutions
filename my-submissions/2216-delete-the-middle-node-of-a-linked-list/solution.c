8a33d62119353574cf9fcb30357bf9ef932d58a3
michael7412541 <michael7412541@gmail.com>
2026-06-15 14:48:28 +0000

    Sync LeetCode submission Runtime - 3 ms (44.41%), Memory - 83.1 MB (56.54%)

--- my-submissions/2216-delete-the-middle-node-of-a-linked-list/solution.c
+++ my-submissions/2216-delete-the-middle-node-of-a-linked-list/solution.c
@@ -6,17 +6,20 @@
  * };
  */
 struct ListNode* deleteMiddle(struct ListNode* head) {
-    if(head == NULL || head->next == NULL)
+   if(head == NULL || head->next == NULL)
         return NULL;
-    struct ListNode *slow = head, *fast = head, *prev = NULL;
-    while(fast != NULL && fast->next != NULL){
-        fast = fast->next->next;
-        prev = slow;
-        slow = slow->next;
-    }
-    prev->next = slow->next;
-    free(slow);
 
-    return head;
+   struct ListNode *slow, *fast, *prev = NULL;
+   slow = head;
+   fast = head;
+   while(fast != NULL  && fast->next != NULL){
+    prev = slow;
+    slow = slow->next;
+    fast = fast->next->next;
+   }
+   prev->next = slow->next;
+   free(slow);
+
+   return head;
 
 }
