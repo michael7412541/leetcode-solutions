e9def83ea4a9762eab10ba6b7e8568d9c43e92e9
michael7412541 <michael7412541@gmail.com>
2026-06-17 14:19:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.1 MB (86.54%)

--- my-submissions/0695-max-area-of-island/solution.c
+++ my-submissions/0695-max-area-of-island/solution.c
@@ -1,50 +1,28 @@
-typedef struct {
-    int x;
-    int y;
-} Point;
+int dfs(int **grid, int x, int y, int x_size, int y_size){
+    if(x >= x_size || y >= y_size || x < 0 || y < 0)
+        return 0;
+    
+    if(grid[x][y] == 0)
+        return 0;
+    grid[x][y] = 0;
+    int up = dfs(grid, x + 1, y, x_size, y_size);
+    int down = dfs(grid, x - 1, y, x_size, y_size);
+    int left = dfs(grid, x, y + 1, x_size, y_size);
+    int right = dfs(grid, x, y - 1, x_size, y_size);
+    
+    return up + down + left + right + 1;
+
+}
 int maxAreaOfIsland(int** grid, int gridSize, int* gridColSize) {
-    int **record = malloc(sizeof(int*)*gridSize);
-    int i, j, size, answer = 0, head = -1, tail = -1, x1, y1;
-    int dir[4][2] = {{1,0}, {-1, 0}, {0, 1}, {0,-1}};
-    for(i = 0; i < gridSize; i++){
-        record[i] = malloc(sizeof(int)*gridColSize[i]);
-    }
-    // record init to 0
-    for(i = 0; i < gridSize; i++){
-        for(j = 0; j < gridColSize[i]; j++){
-            record[i][j] = 0;
-        }
-    }
-    
-    Point queue[1000000], cur;
-    
-    for(i = 0; i < gridSize; i++){
-        for(j = 0; j < gridColSize[i]; j++){
-            size = 0;
-            if(record[i][j] != 1 && grid[i][j] == 1){
-                head = -1;
-                tail = -1;
-                queue[++tail] = (Point){i, j};
-                size = 0;
-                record[i][j] = 1; 
-                while(head < tail){
-                    cur = queue[++head];
-                    record[cur.x][cur.y] = 1; 
-                    for(int k = 0; k < 4; k++){
-                        x1 = cur.x + dir[k][0];
-                        y1 = cur.y + dir[k][1];
-                        if(x1 >= 0 && x1 < gridSize && 
-                           y1 >= 0 && y1 < gridColSize[0] && 
-                           record[x1][y1] != 1 && grid[x1][y1] == 1){
-                            record[x1][y1] = 1; 
-                            queue[++tail] = (Point){x1, y1};
-                        }
-                    }
-                    size++;
-                }
-                if(size > answer)
-                    answer = size;
+    int answer = 0, area;
+    for(int i = 0; i < gridSize; i++){
+        for(int j = 0; j < gridColSize[i]; j++){
+            if(grid[i][j] == 1){
+                area = dfs(grid, i, j, gridSize, gridColSize[i]);
+                if(area > answer)
+                    answer = area;
             }
+               
         }
     }
     return answer;
