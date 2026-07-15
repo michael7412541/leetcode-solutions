3c642208b42d80a682980b94d9df967f859a388a
michael7412541 <michael7412541@gmail.com>
2026-07-15 16:23:21 +0000

    Sync LeetCode submission Runtime - 1 ms (8.49%), Memory - 11.4 MB (5.66%)

--- my-submissions/0871-keys-and-rooms/solution.c
+++ my-submissions/0871-keys-and-rooms/solution.c
@@ -1,20 +1,37 @@
-void dfs(int **rooms, int roomsSize, int *roomsColSize, int *visited, int start){
+typedef struct Edge{
+    int to;
+    struct Edge *next;
+}Edge;
+
+void dfs(Edge **graph, int start, int *visited){
     if(visited[start] == 1)
         return;
     visited[start] = 1;
-    for(int i = 0; i < roomsColSize[start]; i++){
-        dfs(rooms, roomsSize, roomsColSize, visited, rooms[start][i]);
+    for(Edge *e = graph[start]; e ;e = e->next){
+        dfs(graph, e->to, visited);
     }
-    
 }
 bool canVisitAllRooms(int** rooms, int roomsSize, int* roomsColSize) {
     int *visited = malloc(sizeof(int) * roomsSize);
-    memset(visited, 0, sizeof(int) * roomsSize);
-    dfs(rooms, roomsSize, roomsColSize, visited, 0);
-
-    for(int i = 0; i < roomsSize; i++)
+    Edge **graph = malloc(sizeof(Edge*) * roomsSize);
+    for(int i = 0; i < roomsSize; i++){
+        visited[i] = 0;
+        graph[i] = NULL;
+    }
+    for(int i = 0; i < roomsSize; i++){
+        for(int j = 0; j < roomsColSize[i]; j++){
+            int to = rooms[i][j];
+            int from = i;
+            Edge *new = malloc(sizeof(Edge));
+            new->to = to;
+            new->next = graph[from];
+            graph[from] = new;  
+        }
+    }
+    dfs(graph, 0, visited);
+    for(int i = 0; i < roomsSize; i++){
         if(visited[i] == 0)
             return false;
-    
+    }
     return true;
 }
