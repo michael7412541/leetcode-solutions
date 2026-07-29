2eae9c1c55ef2e5eb8abbd4bc03129f039afa3c3
michael7412541 <michael7412541@gmail.com>
2026-07-29 15:37:42 +0000

    Sync LeetCode submission Runtime - 2 ms (45.10%), Memory - 11.5 MB (79.41%)

--- my-submissions/0496-next-greater-element-i/solution.c
+++ my-submissions/0496-next-greater-element-i/solution.c
@@ -2,25 +2,32 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* nextGreaterElement(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
-    int stack[2000] = {0}, record[10000] = {0};
-    int i = 0, top = -1;
-    int *answer = malloc(sizeof(int) * nums1Size);
-    for(i = 0; i < nums2Size; i++){
-        while(top != -1 && nums2[i] > nums2[stack[top]]){
-            record[nums2[stack[top]]] = nums2[i];
-            //printf("top = %d\n", top);
+    int top = -1, stack[nums2Size];
+    memset(stack, 0, sizeof(stack));
+    *returnSize = nums1Size;
+    int idx, *answer = malloc(sizeof(int) * nums1Size), *next = malloc(sizeof(int) * nums2Size);
+    memset(next, -1, sizeof(int) * nums2Size);
+    stack[++top] = 0;
+
+    for(int i = 1; i < nums2Size; i++){
+
+        while(top >= 0 && nums2[stack[top]] < nums2[i]){
+            next[stack[top]] = i;
             top--;
-            //pop
         }
-        ///push
         stack[++top] = i;
     }
-    for(i = 0; i < nums1Size; i++){
-        if(record[nums1[i]] > 0)
-            answer[i] = record[nums1[i]];
-        else
-            answer[i] = -1;
+
+    
+
+    for(int i = 0; i < nums1Size; i++){
+        for(int j = 0; j < nums2Size; j++){
+            if(nums1[i] == nums2[j]){
+                answer[i] = next[j] >= 0 ? nums2[next[j]] : -1;
+                break;
+            }
+
+        }
     }
-    *returnSize = nums1Size;
     return answer;
 }
