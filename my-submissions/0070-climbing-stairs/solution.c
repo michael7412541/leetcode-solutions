dc8d112e81e5cbce1ab76d7b645a52a905c48e91
michael7412541 <michael7412541@gmail.com>
2026-07-30 16:45:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (5.59%)

--- my-submissions/0070-climbing-stairs/solution.c
+++ my-submissions/0070-climbing-stairs/solution.c
@@ -1,41 +1,14 @@
-int calculate(int n, int *hash) {
-    if(hash[n] != 0) {
-        return hash[n];
-    }
-    else if(n == 1 || n == 2) {
-        hash[n] = n;
-        return hash[n];
-    }
-    else {
-        hash[n] = calculate(n - 1, hash) + calculate(n - 2, hash);
-        return hash[n];
-    }
-}
 int climbStairs(int n) {
-    int *hash = (int*)calloc(46, sizeof(int));
-    int result = calculate(n, hash);
-    free(hash);
-    return result;
-    /*int sum = 0, x = 0, y = 0;
-    long long res = 1;
-    for(y = 0; y <= n/2; y++) {
-        if(n == 1)
-            return 1;
-        if(n == 2)
-            return 2;
-        
-        x = n - 2* y;
-        
-        if(x == 0 || y == 0)
-            sum += 1;
-        else {
-            res = 1;
-            //calculate Cx+y y
-            for (int i = 1; i <= x; i++) {
-                    res = res * (y + i) / i;
-            }
-            sum += res;
-        }
+    if(n < 3)
+        return n;
+    int p1, p2, c;
+    
+    p1 = 1;
+    p2 = 2;
+    for(int i = 3; i <= n; i++){
+        c = p1 + p2;
+        p1 = p2;
+        p2 = c; 
     }
-    return sum;*/
+    return c;
 }
