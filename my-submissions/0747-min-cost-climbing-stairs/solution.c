04abd94e456f2d170b98f9112c79fe40348079aa
michael7412541 <michael7412541@gmail.com>
2026-08-03 15:26:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.1 MB (56.23%)

--- my-submissions/0747-min-cost-climbing-stairs/solution.c
+++ my-submissions/0747-min-cost-climbing-stairs/solution.c
@@ -1,13 +1,19 @@
 int min(int a, int b){
-    return a > b ? b : a;
+    return a < b ? a : b;
 }
 int minCostClimbingStairs(int* cost, int costSize) {
-    int dp[costSize + 1];
-    dp[0] = 0;
-    dp[1] = 0;
+    int dp[costSize+1], a, b, c;
+    //dp[0] = 0;
+    a = 0;
+    //dp[1] = 0;
+    b = 0;
     for(int i = 2; i <= costSize; i++){
-        dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
-    }
+        //dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
 
-    return dp[costSize];
-} 
+        c = min(a + cost[i-2], b + cost[i-1]);
+        a = b;
+        b = c;
+
+    }
+    return c;//dp[costSize];
+}
