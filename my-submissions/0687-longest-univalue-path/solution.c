4339f4a933a551677ae1ed47bc234d7475a36a32
michael7412541 <michael7412541@gmail.com>
2026-08-22 06:05:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 55.1 MB (60.00%)

--- my-submissions/0687-longest-univalue-path/solution.c
+++ my-submissions/0687-longest-univalue-path/solution.c
@@ -6,38 +6,44 @@
  *     struct TreeNode *right;
  * };
  */
-int max = 0;
-int helper(struct TreeNode* root, int val){
-    if(root == NULL)
-        return 0;
-    
-    if(root->val == val){
-        int left = helper(root->left, val);
-        int right = helper(root->right, val);
-    
-        if(left + right > max)
-            max = left + right;
+int answer;
+//state定義為跟這個node值相同的longest path
+struct State{
+    int path;
+};
+int max(int a, int b ,int c){
+    if(a >= b && a >= c) return a;
+    else if(b >= a && b >= c) return b;
+    else return c;
+}
+struct State dfs(struct TreeNode *node){
+    struct State result;
+    if(node == NULL) return (struct State){0};
 
-        return (left > right ? left : right) + 1;
-    }
-    else{
-        int left = helper(root->left, root->val);
-        int right = helper(root->right, root->val);
-    
-        if(left + right > max)
-            max = left + right;
+    struct State left = dfs(node->left);
+    struct State right = dfs(node->right);
+    result.path = 0;
 
-        return 0;
+    if(node->left != NULL && node->val == node->left->val)
+        result.path = left.path + 1;
+   if(node->right != NULL && node->val == node->right->val) {
+        int right_path_val = right.path + 1;
+        if(right_path_val > result.path)
+            result.path = right_path_val;
     }
-   
-    
+    int temp = 0;
+
+    if(result.path != 0 && node->left && node->right && node->left->val == node->right->val && node->left->val == node->val)
+        temp = left.path + right.path + 2;
+    answer = max(result.path, temp, answer);
+
+    return result;
 }
 
-
 int longestUnivaluePath(struct TreeNode* root) {
-    max = 0;
     if(root == NULL)
         return 0;
-    helper(root, root->val);
-    return max;
+    answer = 0;
+    struct State result = dfs(root);
+    return answer;
 }
