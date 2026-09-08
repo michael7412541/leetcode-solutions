74a995806ded474a58589a72e2af83a471c54d86
michael7412541 <michael7412541@gmail.com>
2026-09-08 15:33:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.9 MB (24.16%)

--- my-submissions/0090-subsets-ii/solution.c
+++ my-submissions/0090-subsets-ii/solution.c
@@ -6,22 +6,23 @@
 #define MAX_SIZE 1024
 
 void dfs(int **result, int *nums, int numsSize, int *returnSize, int **returnColumnSizes, int *path, int pathsize, int index){
-    path[pathsize++] = nums[index];
-
+    
     result[(*returnSize)] = malloc(sizeof(int) * pathsize);
     (*returnColumnSizes)[(*returnSize)] = pathsize;
     for(int i = 0; i < pathsize; i++){
         result[(*returnSize)][i] = path[i];
     }
     (*returnSize)++;
-    
-    for(int i = index + 1; i < numsSize; i++){
-        if(i > index + 1 && nums[i] == nums[i-1])
-            continue;
-        
-        dfs(result, nums, numsSize, returnSize, returnColumnSizes, path, pathsize, i);
-    }
 
+    
+    for(int i = index; i < numsSize; i++){
+        if(i > index && nums[i] == nums[i-1])
+            continue;
+        path[pathsize++] = nums[i];
+        dfs(result, nums, numsSize, returnSize, returnColumnSizes, path, pathsize, i+1);
+        pathsize--;
+    }
+    
 }
 int compare(const void *a, const void *b){
     return *(int*)a - *(int*)b;
@@ -31,20 +32,14 @@ int compare(const void *a, const void *b){
 int** subsetsWithDup(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
     int **result = malloc(sizeof(int*) * MAX_SIZE);
     int *path = malloc(sizeof(int*) * numsSize);
-    *returnSize = 1;
+    *returnSize = 0;
     (*returnColumnSizes) = malloc(sizeof(int) * MAX_SIZE);
-    (*returnColumnSizes)[0] = 0;
     
     qsort(nums, numsSize, sizeof(int), compare);
 
-    for(int i = 0; i < numsSize; i++){
-        if(i == 0){
-            dfs(result, nums, numsSize, returnSize, returnColumnSizes, path, 0, i);
-        }
-        else if(nums[i] != nums[i-1]){
-            dfs(result, nums, numsSize, returnSize, returnColumnSizes, path, 0, i);
-        }
-    }
+
+    dfs(result, nums, numsSize, returnSize, returnColumnSizes, path, 0, 0);
+
     free(path);
     return result;    
 }
